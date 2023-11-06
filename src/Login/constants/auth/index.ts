8883694efe5d6faea757e67fc812402055c86f31
TheGiantKiller <ktsdsd94@gmail.com>
2023-11-06 17:17:58 +0900

    fix : redirect uri 수정

--- src/Login/constants/auth/index.ts
+++ src/Login/constants/auth/index.ts
@@ -1,4 +1,4 @@
-const REDIRECT_URI = `http://localhost:3000/oauth`;
+const REDIRECT_URI = `${process.env.REACT_APP_SERVER_URI}/oauth`;
 const {
   REACT_APP_KAKAO_CLIENT_ID: KAKAO_KEY,
   REACT_APP_GOOGLE_CLIENT_ID: GOOGLE_Client_ID,
